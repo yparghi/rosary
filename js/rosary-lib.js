273628bf984c73aa4d6cdb1b9bd0fa5eb736ab93
Yash Parghi <yash@daybydaycoding.com>
2020-02-20 18:56:01 -0500

    Stub function and button for command parsing

---
+++ js/rosary-lib.js
@@ -0,0 +1,6 @@
+function parseCommand() {
+  // TODO: Pass this in somehow, don't grab it from the DOM. And write tests.
+  input = document.getElementById("gameInput").value;
+  console.log('Parse: ' + input);
+}
+
