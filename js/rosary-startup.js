84951ea58ae30f1db296916bf0c9a5cc66f963c1
Yash Parghi <yash@daybydaycoding.com>
2020-02-20 18:13:55 -0500

    Start stubbing out a webpage for 'staging' games

---
+++ js/rosary-startup.js
@@ -0,0 +1,10 @@
+function rosary_start() {
+  console.log("Rosary starting!");
+  load_initial_game_state();
+}
+
+function load_initial_game_state() {
+  display = document.getElementById("gameDisplay");
+  display.innerHTML = "Started game!";
+}
+
