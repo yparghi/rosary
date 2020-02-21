d6c0675d41930013656774943d9e9f7ff5b1b1a7
Yash Parghi <yash@daybydaycoding.com>
2020-02-20 19:11:46 -0500

    Parsing: String splitting code

--- js/rosary-lib.js
+++ js/rosary-lib.js
@@ -1,6 +1,24 @@
 function parseCommand() {
   // TODO: Pass this in somehow, don't grab it from the DOM. And write tests.
-  input = document.getElementById("gameInput").value;
-  console.log('Parse: ' + input);
+  input = document.getElementById("gameInput").value.toLowerCase();
+  doInternalParsing(input);
 }
+
+function doInternalParsing(text) {
+  words = text.split(/\s+/)
+  words = words.filter(function(word) {
+    return !PREPOSITIONS.includes(word);
+  });
+  console.log('Words: ', words);
+
+  parseCommandFromWords(words);
+}
+
+function parseCommandFromWords(words) {
+  verb = words[0];
+}
+
+const PREPOSITIONS = [
+  "to",
+];
 
